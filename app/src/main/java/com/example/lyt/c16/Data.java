0d737b838238c76b5d11f0607a18ebeba6a55f9a
unknown <>
2016-09-08 09:53:52 +0800

    [上机实践]进一步熟悉Intent Filter 注意与前面的上机实践（使用Intent隐式启动）略有不同，熟悉data等元素的使用方法 1.熟悉intent filter的含义与作用 2.熟悉action，category、data等用法 3.理解Android对intent filter的处理与匹配过程 4.将代码上传到github上。 5.将遇到的问题、分析与理解、知识点小结等上传到gitbook上去。

---
+++ app/src/main/java/com/example/lyt/c16/Data.java
@@ -0,0 +1,14 @@
+package com.example.lyt.c16;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by liyutong on 2016/9/8.
+ */
+public class Data extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data);
+}}
